0e66f1ebc88a0e442ddae9d0603bab1b2c9408b1
sadman_ichha <sadmanichha1@gmail.com>
2023-02-21 20:53:20 +0600

    appbar & preferredSize

--- lib/widgets/appbar_preferredSize.dart
+++ lib/widgets/appbar_preferredSize.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
 class AppBar_PreferredSize extends StatelessWidget {
-  const AppBar_PreferredSize({super.key});
+  // const AppBar_PreferredSize({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: PreferredSize(
+          child: AppBar(
+            title: Text("Flutter Avenue"),
+            centerTitle: true,
+            backgroundColor: Colors.purple,
+            leading:
+                IconButton(onPressed: () {}, icon: Icon(Icons.view_agenda)),
+            actions: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+            ],
+          ),
+          preferredSize: Size.fromHeight(120.0)),
     );
   }
 }
