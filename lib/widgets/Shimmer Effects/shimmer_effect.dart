3928fcde73e69c5e83f268a2f20940372a049669
sadman_ichha <sadmanichha1@gmail.com>
2023-02-23 20:20:20 +0600

    shimmer effect

--- lib/widgets/Shimmer Effects/shimmer_effect.dart
+++ lib/widgets/Shimmer Effects/shimmer_effect.dart
@@ -11,32 +11,45 @@ class SimmerEffect extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.purple,
       ),
-      body: Container(
-        height: 150.0,
-        color: Colors.grey.withOpacity(0.3),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const FlutterLogo(size: 60.0),
-            const SizedBox(width: 4.0),
-            Expanded(
-                child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text(
-                  "This is Lorem ipsum!",
-                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+      body: ListView.builder(
+          itemCount: 15,
+          itemBuilder: (context, int index) {
+            return Container(
+              height: 150.0,
+              margin: const EdgeInsets.only(bottom: 10.0),
+              color: Colors.grey.withOpacity(0.3),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 12.0, right: 2.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const FlutterLogo(size: 50.0),
+                    const SizedBox(width: 30.0),
+                    Expanded(
+                        child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          "This is Lorem ipsum!",
+                          style: TextStyle(
+                              fontSize: 20.0, fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(height: 2.0),
+                        Text(
+                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley e ",
+                          style: TextStyle(
+                              fontSize: 15.0,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black87),
+                        ),
+                      ],
+                    ))
+                  ],
                 ),
-                Text(
-                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley e ",
-                  style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w400),
-                ),
-              ],
-            ))
-          ],
-        ),
-      ),
+              ),
+            );
+          }),
     );
   }
 }
