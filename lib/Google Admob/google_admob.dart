4d011aeede20ebc44327fd764fe793b181b5f0b4
sadman_ichha <sadmanichha1@gmail.com>
2023-02-24 12:30:33 +0600

    Google admob

--- lib/Google Admob/google_admob.dart
+++ lib/Google Admob/google_admob.dart
@@ -1,7 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class GoogleAdmob extends StatelessWidget {
-  const GoogleAdmob({super.key});
+  // const GoogleAdmob({super.key});
+
+  final BannerAd myBanner = BannerAd(
+    adUnitId: Platform.isAndroid
+        ? 'ca-app-pub-3940256099942544/6300978111'
+        : 'ca-app-pub-3940256099942544/2934735716',
+    size: AdSize.banner,
+    request: AdRequest(),
+    listener: BannerAdListener(),
+  );
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_avenue/Google%20Admob/google_admob.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   runApp(const MyApp());
 }
 
