ca8d65d40742d6d48f83786bee4f04ca6c629572
sadman_ichha <sadmanichha1@gmail.com>
2023-02-24 15:36:41 +0600

    Google admob

--- lib/Google Admob/google_admob.dart
+++ lib/Google Admob/google_admob.dart
@@ -1,19 +1,53 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-class GoogleAdmob extends StatelessWidget {
-  // const GoogleAdmob({super.key});
+class GoogleAdmob extends StatefulWidget {
+  @override
+  State<GoogleAdmob> createState() => _GoogleAdmobState();
+}
 
+class _GoogleAdmobState extends State<GoogleAdmob> {
+  // const GoogleAdmob({super.key});
   final BannerAd myBanner = BannerAd(
-    adUnitId: Platform.isAndroid
-        ? 'ca-app-pub-3940256099942544/6300978111'
-        : 'ca-app-pub-3940256099942544/2934735716',
+    adUnitId: 'ca-app-pub-3940256099942544/6300978111',
     size: AdSize.banner,
     request: AdRequest(),
-    listener: BannerAdListener(),
+    listener: BannerAdListener(
+      // Called when an ad is successfully received.
+      onAdLoaded: (Ad ad) {
+        print(ad.toString());
+        Fluttertoast.showToast(msg: "Ads loaded");
+      },
+
+      // Called when an ad request failed.
+      onAdFailedToLoad: (Ad ad, LoadAdError error) {
+        // Dispose the ad here to free resources.
+        ad.dispose();
+        Fluttertoast.showToast(msg: 'Ad failed to load: $error');
+        print('Ad failed to load: $error');
+      },
+
+      // Called when an ad opens an overlay that covers the screen.
+      onAdOpened: (Ad ad) {
+        print('Ad opened.');
+        Fluttertoast.showToast(msg: "Ads opened");
+      },
+
+      // Called when an ad removes an overlay that covers the screen.
+      onAdClosed: (Ad ad) => print('Ad closed.'),
+      // Called when an impression occurs on the ad.
+      onAdImpression: (Ad ad) => print('Ad impression.'),
+    ),
   );
+
+  @override
+  void initState() {
+    myBanner.load();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +56,12 @@ class GoogleAdmob extends StatelessWidget {
         title: const Text("Table & DataTable"),
         centerTitle: true,
         backgroundColor: Colors.purple,
+      ),
+      body: Center(
+        child: Container(
+          height: 50.0,
+          child: AdWidget(ad: myBanner),
+        ),
       ),
     );
   }
