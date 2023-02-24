b07a3a5894276b115f9286035eee41f82d574476
sadman_ichha <sadmanichha1@gmail.com>
2023-02-24 11:42:37 +0600

    google admob

---
+++ lib/Google Admob/google_admob.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class GoogleAdmob extends StatelessWidget {
+  const GoogleAdmob({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Table & DataTable"),
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'widgets/Table & DataTable/table_dataTable.dart';
+import 'package:flutter_avenue/Google%20Admob/google_admob.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return const MaterialApp(
       title: "Flutter Avenue",
       debugShowCheckedModeBanner: false,
-      home: TableAndDataTable(),
+      home: GoogleAdmob(),
     );
   }
 }
