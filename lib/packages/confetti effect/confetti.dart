679496d3f0266b6959259e8389df1d57337543fe
sadman_ichha <sadmanichha1@gmail.com>
2023-05-22 11:54:40 +0600

    Used: Confetti Effect

--- lib/packages/confetti effect/confetti.dart
+++ lib/packages/confetti effect/confetti.dart
@@ -1,11 +1,45 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:confetti/confetti.dart';
+import 'package:flutter/material.dart';
 
-class ConfettiEffect extends StatelessWidget {
-  const ConfettiEffect({super.key});
+class ConfettiEffect extends StatefulWidget {
+  @override
+  State<ConfettiEffect> createState() => _ConfettiEffectState();
+}
+
+class _ConfettiEffectState extends State<ConfettiEffect> {
+  //const ConfettiEffect({super.key});
+  final ConfettiController _confettiController = ConfettiController();
+
+  bool isPlaying = false;
+  @override
+  void dispose() {
+    _confettiController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Scaffold(
+          appBar: AppBar(),
+          body: Center(
+            child: ElevatedButton(
+              onPressed: () {
+                if (isPlaying) {
+                  _confettiController.stop();
+                } else {
+                  _confettiController.play();
+                }
+                isPlaying = !isPlaying;
+              },
+              child: const Text("Party Time"),
+            ),
+          ),
+        ),
+        ConfettiWidget(confettiController: _confettiController)
+      ],
+    );
   }
 }
